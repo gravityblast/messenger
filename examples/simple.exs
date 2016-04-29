bd8889363a5c05846465588fce13b1a6e4613687
Andrea Franz <andrea@gravityblast.com>
2016-04-29 17:45:19 +0100

    add a simple example script

---
+++ examples/simple.exs
@@ -0,0 +1,21 @@
+#!/usr/bin/env mix run
+
+alias Messenger.Recipient
+alias Messenger.Message
+alias Messenger.Request
+
+page_access_token = System.get_env "PAGE_ACCESS_TOKEN"
+my_user_id = System.get_env "MY_USER_ID"
+
+recipient = Recipient.new
+            |> Recipient.put_id(my_user_id)
+
+message = Message.new
+          |> Message.put_text("Hello world")
+
+resp = Request.new
+        |> Request.put_recipient(recipient)
+        |> Request.put_message(message)
+        |> Request.send(page_access_token)
+
+IO.puts (inspect resp)
