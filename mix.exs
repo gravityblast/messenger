664fcbe42368f9b8b41710a1b7f0d131c94c99a2
Andrea Franz <andrea@gravityblast.com>
2016-05-01 00:40:29 +0100

    increase version

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Messenger.Mixfile do
 
   def project do
     [app: :messenger,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.2",
      name: "Messenger",
      description: @description,
